e2f04f980ceaae96a039c2649e3a70d532ddab17
tilltheant <58863820+tilltheant@users.noreply.github.com>
2020-10-29 13:29:10 +0100

    Title change

--- Modules en tryout met data/Sorteren.js
+++ Modules en tryout met data/Sorteren.js
@@ -1,8 +1,10 @@
 
+//Scannen over een array en daaruit een ID halen
 export function filteren (data, column){
   return data.map(uitkomst => uitkomst[column])
 }
 
+//Tellen hoeveel een gekozen item voorkomt in de array (Code van de les gerecycled!)
 export function tellenmaar (kieseenplek, specefiekedag){
   let count = 0
   kieseenplek.forEach(item => {
--- Modules en tryout met data/api.js
+++ Modules en tryout met data/api.js
@@ -1,3 +1,5 @@
+//Data fetchen en ophalen in json format
+
 export async function dataophalen(url) {
   const antwoord = await fetch(url)
   const data = await antwoord.json (url)
